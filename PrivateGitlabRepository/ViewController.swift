947b480ad058cc188602099d272dc28d8a6d628b
denrb <32716620+denrb@users.noreply.github.com>
2018-08-17 15:43:01 +0300

    Initial Commit

---
+++ PrivateGitlabRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivateGitlabRepository
+//
+//  Created by Den on 8/17/18.
+//  Copyright © 2018 MicroGameDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
